7a0a81afd1b6f8a57c75f7555421b7e5d6542355
Josué Durand Pebe <josue.durand8@gmail.com>
2024-05-23 16:33:35 -0500

    requestRuntimePermission

--- app/src/main/java/com/durand/musicplayerjd/MainActivity.kt
+++ app/src/main/java/com/durand/musicplayerjd/MainActivity.kt
@@ -2,17 +2,24 @@ package com.durand.musicplayerjd
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.durand.musicplayerjd.databinding.ActivityFavoriteBinding
 import com.durand.musicplayerjd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    override fun onStart() {
+        requestRuntimePermission()
+        super.onStart()
+    }
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +45,37 @@ class MainActivity : AppCompatActivity() {
         binding.playlistButton.setOnClickListener {
             startActivity(Intent(this, PlayListActivity::class.java))
         }
-
     }
+
+    private fun requestRuntimePermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                13,
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+        else
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                13,
+            )
+    }
+
 }
